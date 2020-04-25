4e0e821927fd52218b5189235b32b8a55ec2883e
dkrut <dd.krutikov@gmail.com>
2020-04-25 15:21:21 +0300

    refactoring

--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,6 @@
 public class Main {
     public static void main(String[] args) {
         JavaSoundRecorder recorder = new JavaSoundRecorder();
-
-        // creates a new thread that waits for a specified
-        // of time before stopping
-        Thread stopper = new Thread(new Runnable() {
-            public void run() {
-                try {
-                    Thread.sleep(RECORD_TIME);
-                } catch (InterruptedException ex) {
-                    ex.printStackTrace();
-                }
-                recorder.finish();
-            }
-        });
-
-        stopper.start();
-
-        // start recording
-        recorder.start();
+        recorder.recordSound(10000, "out/file.wav");
     }
 }
