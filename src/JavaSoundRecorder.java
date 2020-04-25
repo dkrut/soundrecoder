47d4b6f5338f6335792b06385bf449619713f7b3
dkrut <dd.krutikov@gmail.com>
2020-04-25 14:48:56 +0300

    move main method to Main class

--- src/JavaSoundRecorder.java
+++ src/JavaSoundRecorder.java
@@ -73,29 +73,4 @@ public class JavaSoundRecorder {
         line.close();
         System.out.println("Finished");
     }
-
-    /**
-     * Entry to run the program
-     */
-    public static void main(String[] args) {
-        final JavaSoundRecorder recorder = new JavaSoundRecorder();
-
-        // creates a new thread that waits for a specified
-        // of time before stopping
-        Thread stopper = new Thread(new Runnable() {
-            public void run() {
-                try {
-                    Thread.sleep(RECORD_TIME);
-                } catch (InterruptedException ex) {
-                    ex.printStackTrace();
-                }
-                recorder.finish();
-            }
-        });
-
-        stopper.start();
-
-        // start recording
-        recorder.start();
-    }
 }
---
+++ src/Main.java
@@ -0,0 +1,27 @@
+/**
+ * Created by Denis Krutikov on 25.04.2020.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        final JavaSoundRecorder recorder = new JavaSoundRecorder();
+
+        // creates a new thread that waits for a specified
+        // of time before stopping
+        Thread stopper = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    Thread.sleep(RECORD_TIME);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+                recorder.finish();
+            }
+        });
+
+        stopper.start();
+
+        // start recording
+        recorder.start();
+    }
+}
